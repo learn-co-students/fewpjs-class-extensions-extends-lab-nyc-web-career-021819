5733674e4ea518ebcfbe6220ed607185e8f44c72
github username <github email address>
2019-04-13 22:43:18 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,56 @@
 // Your code here
+class Polygon {
+  constructor(array) {
+    this.array = array
+    this.count = this.array.length
+  }
+  get perimeter() {
+    let perimeter = 0
+
+    this.array.forEach(function(side) {
+      perimeter += side
+    })
+    return perimeter
+
+  }
+  get getCount() {
+    return this.array.length
+
+  }
+}
+
+class Triangle extends Polygon {
+
+  get isValid() {
+  		if (
+  			this.array[0] + this.array[1] <= this.array[2] ||
+  			this.array[0] + this.array[2] <= this.array[1] ||
+  			this.array[1] + this.array[2] <= this.array[0]
+  		) {
+  			return false
+  		} else {
+  			return true
+  		}
+  	}
+  }
+class Square extends Polygon {
+
+  get isValid() {
+		function getSum(total, num) {
+			return total + num
+		}
+		if (
+			this.array[0] === this.array[1] &&
+			this.array[1] === this.array[2] &&
+			this.array[2] === this.array[3]
+		) {
+			return true
+		} else {
+			return false
+		}
+	}
+
+	get area() {
+		return this.array[0] ** 2
+	}
+}
